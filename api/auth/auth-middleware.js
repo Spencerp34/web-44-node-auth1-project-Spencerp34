5409e9e9fa9d588eb69080756364ecd9610f317d
Spencerp34 <spencerp34@yahoo.com>
2021-08-16 16:45:09 -0600

    got all middleware workin

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -49,15 +49,20 @@ async function checkUsernameFree(req, res, next) {
   }
 */
 async function checkUsernameExists(req, res, next) {
-  const {username} = req.body
-  const result = await User.findBy({username})
-  const exists = result[0].user
+  try{
+    const {username} = req.body
+    const result = await User.findBy({username})
 
-  if(exists){
-    next()
-  }else{
-    next({status:401, message: 'Invalid Credentials'})
+    if(result.length){
+      next()
+    }else{
+      next({status:401, message: 'Invalid Credentials'})
+    }
+  }catch(err){
+    next(err)
   }
+  
+
 }
 
 /*
@@ -69,16 +74,13 @@ async function checkUsernameExists(req, res, next) {
   }
 */
 async function checkPasswordLength(req, res, next) {
-  // const {password} = req.body
-  // const result = await User.findBy({password})
-  // const exists = result[0].password
+  const {password} = req.body
 
-  // if(!exists || exists.length < 3){
-  //   next({status:422, message: 'Password must be longer than 3 chars'})
-  // }else{
-  //   next()
-  // }
-  next()
+  if(!password || password.length < 3){
+    next({status:422, message: 'Password must be longer than 3 chars'})
+  }else{
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
